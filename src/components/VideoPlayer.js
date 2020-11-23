bee090218c17820aca8e5c88c3402d473e08a678
dawa93 <dawa93@naver.com>
2020-11-23 14:36:06 +0900

    remove api key again

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const VideoPlayer = ({ video }) => {
   // React.Fragment 로 반환함. null도 가능.
-  if(!Object.keys(video).length) return <></>; 
+  if (!Object.keys(video).length) return <></>;
 
   return (
     <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item"
-        src={"https://www.youtube.com/embed/" + video.id.videoId} allowFullScreen></iframe>
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe
+          className="embed-responsive-item"
+          src={"https://www.youtube.com/embed/" + video.id.videoId}
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="video-player-details">
+        <h3>{video.snippet.title}</h3>
+        <div>{video.snippet.description}</div>
+      </div>
     </div>
-    <div className="video-player-details">
-      <h3>{video.snippet.title}</h3>
-      <div>{video.snippet.description}</div>
-    </div>
-    </div>
-  ); 
+  );
 };
 
 export default VideoPlayer;
